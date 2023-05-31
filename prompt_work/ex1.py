f293021c16c12d2f5c6ea752ec48bf09b618dbbd
Saffy127 <88598834+Saffy127@users.noreply.github.com>
2023-05-31 14:43:49 -0600

    Create ex1.py

---
+++ prompt_work/ex1.py
@@ -0,0 +1,18 @@
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+llm = OpenAI(temperature=0.9)
+
+prompt = PromptTemplate(
+  input_variables=["product"],
+  template="What is a good name for a company that makes {product}?",
+)
+
+chain = LLMChain(llm=llm, prompt=prompt)
+chain.run("Fleshlights")
+
+llm.get_num_tokens("What a joke")
+
+
+
