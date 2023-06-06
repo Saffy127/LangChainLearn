d64cc461fc44dc20796e477e36a1696f758f8ae8
Saffy127 <88598834+Saffy127@users.noreply.github.com>
2023-06-05 18:29:45 -0600

    Create agent1.py

---
+++ agents/agent1.py
@@ -0,0 +1,19 @@
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.llms import OpenAI
+
+#First load the language model we're going to use the control the agent.
+llm = OpenAI(temperature=0)
+
+#Next, let's load some tools to use. Note that the llm-math tool uses an LLM, so we need to pass that in.
+
+tools = load_tools(["serpapi", "llm-math"], llm=llm)
+
+# Finally, let's initialize an agent with the tools, the language model, and the type of agent we want to use.
+
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+
+
+agent.run("Who is Leo DiCaprio's girlfriend? What is her current age raised to the 0.43 power?")
+
